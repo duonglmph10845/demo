284af390cc10a0578c09becf1390e39119129dd5
HTA <anhht9748@gmail.com>
2021-11-02 15:24:27 +0700

    phân quyền

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -8,13 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     use HasFactory;
-    protected $table = 'permission';
+    protected $table = 'permissions';
     protected $primaryKey = 'id';
     protected $fillable = [
         'permission',
-        
+
     ];
-
-    
-
+    public function getRole(){
+        return $this->belongsToMany(Role::class,'permission_roles');
+    }
 }
